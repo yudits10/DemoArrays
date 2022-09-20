0392980719a1a0e79821e818cb4890a09ced5800
yudits10 <113966685+yudits10@users.noreply.github.com>
2022-09-20 13:36:21 +0700

    Add files via upload

---
+++ DemoArrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DemoArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+
+            var number2 = new int[4] { 1, 3, 5, 7 };
+
+            numbers[0] = 1;
+            numbers[1] = 3;
+            numbers[2] = 5;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            Console.WriteLine(number2[0]);
+            Console.WriteLine(number2[1]);
+            Console.WriteLine(number2[2]);
+        }
+    }
+}
